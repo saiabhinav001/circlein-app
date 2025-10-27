5eaec8287f422c60cd857c0949a183c98a34dbbe
unknown <abhinav.sadineni@gmail.com>
2025-10-27 21:11:11 +0530

    Remove sparkles charm icon from send button in chatbot

--- app/(app)/contact/page.tsx
+++ app/(app)/contact/page.tsx
@@ -500,23 +500,6 @@ export default function ContactPage() {
                               <Send className="w-6 h-6 text-white drop-shadow-lg" />
                             )}
                           </div>
-
-                          {/* Sparkle effect when ready to send */}
-                          {!isLoading && input.trim() && (
-                            <motion.div
-                              className="absolute top-1 right-1"
-                              animate={{
-                                scale: [1, 1.2, 1],
-                                opacity: [0.5, 1, 0.5],
-                              }}
-                              transition={{
-                                duration: 1.5,
-                                repeat: Infinity,
-                              }}
-                            >
-                              <Sparkles className="w-3 h-3 text-white" />
-                            </motion.div>
-                          )}
                         </Button>
                       </motion.div>
                     </div>
